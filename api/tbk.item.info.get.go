975a2c993613b8a93b34a3edea527635a0cee351
afternoon xia <afternoon@afternoondeMacBook-Pro.local>
2019-04-23 16:23:49 +0800

    add api tbk.item.info.get

---
+++ api/tbk.item.info.get.go
@@ -0,0 +1,38 @@
+package api
+
+type TaobaoTbkItemInfoGetResponse struct {
+	TbkItemInfoGetResponse *TbkItemInfoResults `json:"tbk_item_info_get_response"`
+}
+
+type TbkItemInfoResults struct {
+	Results *NTbkItem `json:"results"`
+}
+
+type NTbkItem struct {
+	NTbkItem []*TbkItemInfo `json:"n_tbk_item"`
+}
+
+type TbkItemInfo struct {
+	CatName           string              `json:"cat_name"`
+	NumIid            int                 `json:"num_iid"`
+	Title             string              `json:"title"`
+	PictUrl           string              `json:"pict_url"`
+	SmallImages       map[string][]string `json:"small_images"`
+	ReservePrice      string              `json:"reserve_price"`
+	XkFinalPrice      string              `json:"zk_final_price"`
+	UserType          int                 `json:"user_type"`
+	Provcity          string              `json:"provcity"`
+	ItemUrl           string              `json:"item_url"`
+	SellerId          int                 `json:"seller_id"`
+	Volume            int                 `json:"volume"`
+	Nick              string              `json:"nick"`
+	cat_leaf_name     string              `json:"cat_leaf_name"`
+	is_prepay         string              `json:"is_prepay"`
+	ShopDsr           int                 `json:"shop_dsr"`
+	ratesum           int                 `json:"ratesum"`
+	i_rfd_rate        bool                `json:"i_rfd_rate"`
+	h_good_rate       bool                `json:"h_good_rate"`
+	h_pay_rate30      bool                `json:"h_pay_rate30"`
+	free_shipment     bool                `json:"free_shipment"`
+	material_lib_type string              `json:"material_lib_type"`
+}
